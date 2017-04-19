cffdd6bba537d52fb1bb99069ddcc3987dc5787c
Isaac Marinho <isaacmarinho@gmail.com>
2017-04-19 15:50:18 -0300

    - Inclusão do projeto Model (primeiro commit) conforme exemplo do SfisManager; - Ajustes nas configurações do projeto SimpleBookStore.

--- src/SimpleBookStore/Startup.cs
+++ src/SimpleBookStore/Startup.cs
@@ -9,21 +9,40 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Swashbuckle.AspNetCore.Swagger;
 using System.IO;
+using Model.DataModel;
+using Microsoft.Extensions.Configuration;
 
 namespace SimpleBookStore
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-
+            services.AddScoped<Entities>(_ => new Entities("metadata=res://*/DataModel.DataModel.csdl|res://*/DataModel.DataModel.ssdl|res://*/DataModel.DataModel.msl;provider=Oracle.ManagedDataAccess.Client;provider connection string='DATA SOURCE=10.100.100.171:1521/SBSDB;PASSWORD=compal123;USER ID=COMPAL'"));
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new Info { Title = "SimpleBookStore", Version = "v1" });
             });
+
+            // Add framework services.
+            string connectionString =
+                Configuration.GetSection("Database").GetSection("SFIS")["ConnectionString"];
+            services.AddScoped(_ => new Entities(connectionString));            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
